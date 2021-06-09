32077d45e8be9d016fd021abbfbfe1068215714b
aryansgithub <83552423+aryansgithub@users.noreply.github.com>
2021-06-10 03:00:20 +0530

    Fixed a bug
    The "time taken" text wasn't disappearing from the previous game after pressing reset. Now it does.

--- Color Tiles.js
+++ Color Tiles.js
@@ -19,6 +19,7 @@ function resetAction(e) {
     e.target.textContent = "RESET";
     heading.textContent = "COLOR TILES";
     statusText.textContent = "Try to match the 3x3 grid.";
+    timeText.textContent = "";
 
     for(var j = 0; j<9; j++) {
         innerGridTile[j].style.borderColor = "black";
